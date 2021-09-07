4f129413bafc0a3971a49ee8371d61e99b2d3f64
Mirek Kratochvil <exa.exa@gmail.com>
2021-09-07 14:55:04 +0200

    add remote macro for better Distributed compatibility

--- src/base.jl
+++ src/base.jl
@@ -46,7 +46,7 @@ Get a value `val` from a remote `worker`; quoting of `val` works just as with
 `save_at`. Returns a future with the requested value.
 """
 function get_from(worker, val; mod = Main)
-    remotecall(() -> Base.eval(mod, :($val)), worker)
+    remotecall(() -> Base.eval(mod, val), worker)
 end
 
 """
@@ -357,4 +357,42 @@ Decorate the symbol from `dInfo` with prefix and suffix.
 """
 function tmp_symbol(dInfo::Dinfo; prefix = "", suffix = "_tmp")
     return tmp_symbol(dInfo.val, prefix = prefix, suffix = suffix)
+end
+
+"""
+    @remote module expr
+
+A version of [`@remote`](@ref) that adds additional choice of the module for
+scope.
+"""
+macro remote(mod, x)
+    :(Base.eval($mod, $(QuoteNode(x))))
+end
+
+"""
+    @remote expr
+
+In a function that will get evaluated on a remote worker, this ensures the
+evaluation scope of the expression `expr` (usually a variable) is taken on
+the remote side, preventing namespace clash with the local session.
+
+This is mainly useful for making the functions from `Distributed` package (such
+as `pmap` and `remotecall`) work with the data stored by `DistributedData`
+package.
+
+Internally, this is handled by wrapping in `eval`.
+
+# Example
+```
+julia> save_at(2, :x, 321)
+Future(2, 1, 162, nothing)
+
+julia> let x=123
+         remotecall_fetch(() -> x + (@remote x), 2)
+       end
+444
+```
+"""
+macro remote(x)
+    :(@remote Main $x)
 end
